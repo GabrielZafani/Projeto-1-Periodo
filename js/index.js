1c8525e78bc0e8a0bc477fbe9efc059a8d570db0
Gabriel Roberto <gabreil.zafani@gmail.com>
2026-04-11 16:54:26 -0300

    oi

--- js/index.js
+++ js/index.js
@@ -1,28 +1,20 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // função de blur
     function aplicarBlur(ativar) {
         document.body.classList.toggle("bloqueado", ativar);
     }
 
-    // verifica se está no index e Entrada de Dados via Prompt
     const path = window.location.pathname;
     const repo = "/Projeto-1-Periodo/";
 
     if (path === "/" || path === repo || path.endsWith("index.html")) {
-        
         let nomeVisitante = prompt("Bem-vindo ao site! Qual é o seu nome?");
         let idadeVisitante = Number(prompt("Olá, " + nomeVisitante + "! Qual é a sua idade para acessar o conteúdo sensível?"));
-
         let acessoLiberado = idadeVisitante >= 18;
-
-        // salva no navegador
         localStorage.setItem("acessoLiberado", acessoLiberado);
     }
 
-    // pega valor salvo
     let acesso = localStorage.getItem("acessoLiberado");
 
-    // Lógica de Decisão e Feedback
     if (acesso === "true") {
         alert("Acesso liberado! Conteúdo visível.");
         aplicarBlur(false);
@@ -33,22 +25,16 @@ document.addEventListener("DOMContentLoaded", function () {
 
     const botaoTema = document.getElementById("tema-btn");
 
-    // verifica se já tem tema salvo
     let temaSalvo = localStorage.getItem("tema");
-
     if (temaSalvo === "escuro") {
         document.body.classList.add("dark-mode");
         botaoTema.textContent = "Tema Claro";
     }
 
-    // clique no botão
     botaoTema.addEventListener("click", function (e) {
         e.preventDefault();
-
         document.body.classList.toggle("dark-mode");
-
         let modoEscuro = document.body.classList.contains("dark-mode");
-
         if (modoEscuro) {
             botaoTema.textContent = "Tema Claro";
             localStorage.setItem("tema", "escuro");
